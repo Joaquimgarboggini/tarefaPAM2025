5aa3f4178e30ff91fe46b01f195104ed6239fb63
JOAQUIM DE ALMEIDA FABIO GARBOGGINI <joaquim.garboggini@etec.sp.gov.br>
2025-04-11 10:20:44 -0300

    tarefa pam

---
+++ src/components/Result.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Text, StyleSheet } from "react-native";
+
+const Result = ({ imc }) => {
+    return(
+        <Text style={styles.result}>seu imc é {imc}</Text>
+    );
+};
+
+const styles = StyleSheet.create({
+
+    result: {
+        marginTop: 20,
+        fontSize: 24,
+        textAlign: 'center',
+        color: '#333',
+    },
+});
+
+export default Result;
